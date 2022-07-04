d2c169abba742466be2b719063c69219ff12f8e1
jisena.btc <pjohmaths@gmail.com>
2022-07-04 22:19:20 +0300

    feeling powerful

---
+++ chap2/exer.asm
@@ -0,0 +1,24 @@
+    segment .data
+; exercise one
+a1      dw  37
+b1      dw  350
+c1      dw  -65
+d1      dw  -427
+
+; exercise two
+a2      dw  0000001010101010b ; 682
+b2      dw  1111111111101101b ; -19
+c2      dw  0x0101 ; 257
+d2      dw  0xffcc ; -52
+
+; exercise three
+a3      dw  0x015a
+b3      dw  0xfedc
+c3      dw  0x0101
+d3      dw  0xacdc
+
+; exercise four
+a4      dd  1.375
+b4      dd  0.041015625
+c4      dd  -571.3125
+d4      dd  4091.125
