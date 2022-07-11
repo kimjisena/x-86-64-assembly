9d04c156778220d14bfc0d2a25e1d4dafaa3290b
jisena.btc <pjohmaths@gmail.com>
2022-07-11 14:46:52 +0300

    if statement - cmp and jCC

---
+++ chap8/if.asm
@@ -0,0 +1,24 @@
+; C-like if statement
+; if (a < b) {
+;    temp = a;
+;    a = b;
+;    b = temp;
+;}
+        segment     .data
+a       dq          9
+b       dq          7
+temp    dq          0
+
+        segment     .text
+        global      _start
+_start:
+        mov     rax, [a]            ; move a to rax
+        mov     rbx, [b]            ; move b to rbx
+        cmp     rax, rbx            ; set some flags
+        jge     in_order            ; jump to in_order if a >= b
+        mov     [temp], rax         ; move rax to temp (useless instruction)
+        mov     [a], rbx            ; move rbx to a
+        mov     [b], rax            ; move rax to b
+in_order:
+        xor     eax, eax
+        ret
